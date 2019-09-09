2894901dcab50af202b79719a1e17a30e67b7a78
Sam Sweeney <sam.s.sweeney@gmail.com>
2019-09-08 21:10:24 -0400

    Move markup to partials

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,22 @@ const template = require('gulp-template');
 gulp.task('build-html', () => {
   const aboutModal = fs.readFileSync('src/_about-modal.html', 'utf8');
   const head = fs.readFileSync('src/_head.html', 'utf8');
+  const credits = fs.readFileSync('src/_credits.html', 'utf8');
+  const navbar = fs.readFileSync('src/_navbar.html', 'utf8');
+  const socialLinks = fs.readFileSync('src/_social-links.html', 'utf8');
+  const topSection = fs.readFileSync('src/_top-section.html', 'utf8');
   const takeActionModal = fs.readFileSync('src/_take-action-modal.html', 'utf8');
   const exhibitsStr = fs.readFileSync('exhibits.json', 'utf8');
   const exhibits = JSON.parse(exhibitsStr);
   return gulp.src('src/index.html')
     .pipe(template({
+      credits,
       exhibits,
       aboutModal,
+      socialLinks,
       takeActionModal,
+      navbar,
+      topSection,
       head
     }))
     .pipe(gulp.dest('./'))
