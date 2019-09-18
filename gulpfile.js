f6c4f174d0df1980c1bd97faba3f4aa3a8b9e439
Sam Sweeney <sam.s.sweeney@gmail.com>
2019-09-18 03:49:43 -0700

    Switch to using iframe

--- gulpfile.js
+++ gulpfile.js
@@ -56,29 +56,6 @@ gulp.task('build-html', () => {
     .pipe(gulp.dest('./'))
 });
 
-gulp.task('build-iframe-version', () => {
-  const aboutModal = fs.readFileSync('src/_about-modal.html', 'utf8');
-  const head = fs.readFileSync('src/_head.html', 'utf8');
-  const credits = fs.readFileSync('src/_credits.html', 'utf8');
-  const navbar = fs.readFileSync('src/_navbar.html', 'utf8');
-  const socialLinks = fs.readFileSync('src/_social-links.html', 'utf8');
-  const topSection = fs.readFileSync('src/_top-section.html', 'utf8');
-  const takeActionModal = fs.readFileSync('src/_take-action-modal.html', 'utf8');
-  const exhibits = loadExhibits();
-  return gulp.src('src/iframe.html')
-    .pipe(template({
-      credits,
-      exhibits,
-      aboutModal,
-      socialLinks,
-      takeActionModal,
-      navbar,
-      topSection,
-      head
-    }))
-    .pipe(gulp.dest('./'))
-});
-
 gulp.task('build-exhibits-html', function (doneWithTaskCb) {
   const aboutModal = fs.readFileSync('src/_about-modal.html', 'utf8');
   const head = fs.readFileSync('src/_head.html', 'utf8');
@@ -121,6 +98,6 @@ gulp.task('watch', () => {
   gulp.watch(
     ['src/*.html', 'exhibits.tsv'],
     { ignoreInitial: false },
-    gulp.series('build-html', 'build-exhibits-html', 'build-iframe-version')
+    gulp.series('build-html', 'build-exhibits-html')
    );
 });
